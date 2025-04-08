b0dbf3e8eedae7b03cd7a25ceb9a99472d54f721
yanashapka <yana.shapka.test@gmail.com>
2025-04-08 15:10:48 +0300

    homework 3

---
+++ db/models/contact.js
@@ -0,0 +1,23 @@
+import sequelize from '../sequelize.js';
+import {DataTypes} from 'sequelize';
+
+const Contact = sequelize.define('contact', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  favorite: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+});
+
+export default Contact;
